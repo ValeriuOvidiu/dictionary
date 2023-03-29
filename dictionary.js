4a1505c5ed6584119f12bd85816b85e59e8d106c
ValeriuOvidiu <126098277+ValeriuOvidiu@users.noreply.github.com>
2023-03-29 20:11:48 +0300

    Add files via upload

---
+++ dictionary.js
@@ -0,0 +1,17 @@
+const words = [' '];
+function add() {
+    const theText = document.getElementById('inputWord');
+    words.push(theText.value)
+    theText.value = ''
+}
+function find() {
+    const theWord = document.getElementById('inputWordsToFind');
+    let flag = words.indexOf(theWord.value)
+
+    if (flag == -1) {
+        document.getElementById('output').innerHTML = theWord.value + ' does not exists'
+    } else {
+        document.getElementById('output').innerHTML = theWord.value + ' does exist '
+    }
+    theWord.value = ''
+}
